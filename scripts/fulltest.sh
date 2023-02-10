96c7c3b0cb9ee01b93dd429eb2e8252e5c8f3ab9
Chris Binggeli <99316773+GNUNotUsername@users.noreply.github.com>
2023-02-10 11:35:08 +1000

    Fixed bad unpadding in decompression boilerplate

--- scripts/fulltest.sh
+++ scripts/fulltest.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-GTSARGS="extras/example-bin/example.gtirb ../asl-interpreter/prelude.asl ../mra_tools ../asl-interpreter ../basli/example.gts"
+EXBIN="gtirb-semantics/extras/example-bin"
+GTSARGS="extras/example-bin/example.gtirb ../asl-interpreter/prelude.asl ../mra_tools ../asl-interpreter extras/example-bin/example.gts"
 BASLIARGS="compile; run example.gts example.ast.json.t; exit"
 
-echo "This script assumes the directory layout created by gtirb_semantics/scripts/build-all.sh."
+echo "This script assumes the directory layout created by build-all.sh."
 echo "Press enter to continue or ctrl-c if this requirement is not met."
 read
-clear
 cd ../extras/example-bin
 rm example example.ast example.ast.json example.gtirb example.gts
 aarch64-linux-gnu-gcc example.c -o example
 ddisasm example --ir example.gtirb
 cd ../..
 dune exec gtirb_semantics $GTSARGS > extras/example-bin/example.ast
+cp extras/example-bin/example.gts ../basli
 cd ../basli
+echo
 echo $BASLIARGS | sbt
-rm example.gts
-mv example.ast.json.t ../gtirb_semantics/extras/example-bin
-cd ../gtirb_semantics/extras/example-bin
-python3 -mjson.tool hw.ast.json.t > hw.ast.json
-rm hw.ast.json.t
-less hw.ast.json
+mv example.ast.json.t ../$EXBIN
+cd ../$EXBIN
+python3 -mjson.tool example.ast.json.t > example.ast.json
+rm example.ast.json.t
+cat example.ast.json
