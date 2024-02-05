9689a21627e56e4e616141559184afce12cd3be3
rina <k@rina.fyi>
2024-02-05 15:44:34 +1000

    debug-gts: add more information per block, now requires gtirb library.

--- scripts/debug-gts.py
+++ scripts/debug-gts.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 # vim: ts=2 sts=2 et sw=2
 
+# /// script
+# requires-python = ">=3.11"
+# dependencies = [
+#   "gtirb~=2.0.0",
+# ]
+# ///
+
+# Requirements:
+#  gtirb
+
 """
 debug-gts.py [GTS FILE]
 
@@ -9,40 +19,44 @@ an instruction mnemonic alongside each instruction's statement list.
 
 example output:
 {
-  "GBb7LATIS92OQzoZaJodlg==": [
-    {
-      "mov w0, #0                          // =0x0": [
-        "Stmt_Assign(LExpr_Array(LExpr_Var(_R),Expr_LitInt(\"0\")),Expr_LitBits(\"0000000000000000000000000000000000000000000000000000000000000000\"))"
-      ]
-    },
-    {
-      "ret": [
-        "Stmt_Assign(LExpr_Var(BTypeNext),Expr_LitBits(\"00\"))",
-        "Stmt_Assign(LExpr_Var(__BranchTaken),Expr_Var(TRUE))",
-        "Stmt_Assign(LExpr_Var(_PC),Expr_Array(Expr_Var(_R),Expr_LitInt(\"30\")))"
-      ]
-    }
-  ],
+  "XPbERKxyS8CPh9nu9/oe8A==": {
+    "name": "main [entry] [  1/675]",
+    "address": 4204032,
+    "code": [
+      {
+        "adrp x2, #184320": [
+          "Stmt_Assign(LExpr_Array(LExpr_Var(_R),Expr_LitInt(\"2\")),Expr_LitBits(\"0000000000000000000000000000000000000000010000101111000000000000\"))"
+        ]
+      },
+    ]
+  },
 }
 """
 
+
+import io
 import sys
+import uuid
 import json
+try:
+  import gtirb
+except ImportError:
+  print('ERROR: `gtirb` python package not found! to run this script and automatically download dependencies, you can use `pipx`:', file=sys.stderr)
+  print('', file=sys.stderr)
+  print('    pipx run', sys.argv[0], file=sys.stderr)
+  print('', file=sys.stderr)
+  raise
 import base64
 import shutil
-import typing
-import pathlib
+import gtirb.ir
 import argparse
-import tempfile
 import warnings
-import functools
 import subprocess
 import collections
 import collections.abc
-import multiprocessing.pool
 
-proto_json = pathlib.Path(__file__).parent / 'proto-json.py'
-assert proto_json.exists(), f'\'{proto_json}\' not found.  keep this debug-gts.py in the same folder as proto-json.py'
+PROTO_VERSION = gtirb.version.PROTOBUF_VERSION
+
 llvm_mc = shutil.which('llvm-mc')
 assert llvm_mc, "could not find llvm-mc in PATH, check that llvm is installed."
 
@@ -50,7 +64,6 @@ def chunks(lst, n):
     """Yield successive n-sized chunks from lst."""
     for i in range(0, len(lst), n):
         yield lst[i:i + n]
-
 
 def _decode_isns(isns: collections.abc.Iterable[bytes]):
   hex = ' '.join(f'0x{x:02x}' for opcode_bytes in isns for x in opcode_bytes)
@@ -66,9 +79,9 @@ def decode_isns(isns: collections.abc.Iterable[bytes]):
     out |= _decode_isns(x)
   return out 
 
-def do_block(uuid, blk, contents: bytes, sem, isn_names: dict[bytes, str]):
-  blksize = int(blk['code']['size'])
-  off = int(blk['offset'])
+def do_block(uuid: str, blk: gtirb.CodeBlock, contents: bytes, sem, isn_names: dict[bytes, str]):
+  blksize = blk.size
+  off = blk.offset
 
   isize = 32 // 8  # == 4 bytes per instruction
   def slice(i: int):
@@ -87,23 +100,63 @@ def do_block(uuid, blk, contents: bytes, sem, isn_names: dict[bytes, str]):
     for i, sem in enumerate(sem)
   ]
 
-def do_module(mod, isn_names: dict[bytes, str]):
-  sems = mod['aux_data']['ast']['data']
-  sems = base64.b64decode(sems)
+def b64_uuid(uuid: uuid.UUID) -> str:
+  return base64.b64encode(uuid.bytes).decode('ascii')
+
+def compute_friendly_names(mod: gtirb.Module) -> dict[uuid.UUID, str]:
+  funnames = mod.aux_data['functionNames'].data
+  funentries = mod.aux_data['functionEntries'].data
+  funblocks = mod.aux_data['functionBlocks'].data
+
+  out = {}
+  for func, blks in funblocks.items():
+    l = str(len(blks))
+
+    blks = sorted(blks, key=lambda blk: blk.address) # type: ignore
+    for i, blk in enumerate(blks, 1):
+      entry = ' [entry]' if blk in funentries[func] else ''
+      out[blk.uuid] = funnames[func].name + entry + ' [{i:>{w}}/{l}]'.format(i=i, l=l, w=len(l))
+
+  return out
+
+def friendly_block(mod: gtirb.Module, blk: gtirb.Block, with_uuid=False, *, _block_to_func: dict[uuid.UUID, str] = {}):
+  if not _block_to_func:
+    _block_to_func |= compute_friendly_names(mod)
+
+  uuid = blk.uuid
+  prefix = '' if not with_uuid else b64_uuid(uuid) + ' / '
+  if isinstance(blk, gtirb.CodeBlock):
+    return prefix + _block_to_func[uuid]
+  return prefix + f'({type(blk).__name__})'
+
+def do_module(mod: gtirb.Module, isn_names: dict[bytes, str]):
+  sems = mod.aux_data['ast'].data
   sems = json.loads(sems)
-  # print(sems)
 
   gtirb_ids = set()
   sem_ids = set(sems.keys())
   out = {}
-  for sec in mod['sections']:
-    for ival in sec['byte_intervals']:
-      contents = base64.b64decode(ival['contents'])
-      for blk in ival['blocks']:
-        if 'code' not in blk: continue
-        uuid = blk['code']['uuid']
-        out[uuid] = do_block(uuid, blk, contents, sems[uuid], isn_names)
-        gtirb_ids.add(uuid)
+  for sec in mod.sections:
+    for blk in sec.code_blocks:
+        if not isinstance(blk, gtirb.CodeBlock): continue
+
+        uuid = blk.uuid
+
+        b64 = b64_uuid(uuid)
+        friendly = friendly_block(mod, blk)
+        out[b64] = {
+          'name': friendly,
+          'address': blk.address,
+          'code': do_block(friendly, blk, bytes(blk.byte_interval.contents), sems[b64], isn_names), # type: ignore
+          'successors': {
+            friendly_block(mod, x.target, True) : str(x.label) for x in blk.outgoing_edges
+          },
+        }
+        gtirb_ids.add(b64)
+
+  out = list(out.items())
+  out.sort(key=lambda x: x[1]['address'])
+  out = dict(out)
 
   if gtirb_ids != sem_ids:
     warnings.warn(f'semantics and gtirb block uuids differ.\n'
@@ -120,18 +173,20 @@ def main():
                     default=sys.stdout)
   args = argp.parse_args()
 
-  gts_file = args.gts_input
-  with tempfile.TemporaryFile() as f:
-    subprocess.check_call(
-      [proto_json, gts_file],
-      stdout=f)
-    f.seek(0)
-    data = json.load(f)
+  # make a .gtirb file with appropriate magic number
+  bio = io.BytesIO()
+  bio.write(b'GTIRB\0\0' + bytes([PROTO_VERSION]))
+  with open(args.gts_input, 'rb') as f:
+    bio.write(f.read())
+  bio.seek(0)
+
+  ir = gtirb.IR.load_protobuf_file(bio)
+  del bio
 
   out = []
   # traverse protobuf twice. first, to find all isn bytes, then to apply them.
   isns = collections.defaultdict(str)
-  for mod in data['modules']:
+  for mod in ir.modules:
     do_module(mod, isns)
 
   print('decoding', len(isns), 'opcodes...', file=sys.stderr, end=' ', flush=True)
@@ -139,7 +194,7 @@ def main():
   print('done', file=sys.stderr)
   assert len(isn_names) == len(isns), f"llvm-mc instruction count mismatch {len(isn_names)=} {len(isns)=}"
 
-  for mod in data['modules']:
+  for mod in ir.modules:
     out.append(do_module(mod, isn_names))
 
   json.dump(out, args.json_output, indent=2)
